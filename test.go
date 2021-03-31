446753527276eaa33af2925901dbe961d07135d4
Avutzhan Dautov <dautov92@list.ru>
2021-03-31 12:08:39 +0600

    variables

--- test.go
+++ test.go
@@ -2,9 +2,17 @@ package main
 
 import "fmt"
 
+var golang, angular, vuejs bool
+var react string = "react"
+
 func main() {
+	y := "test y"
+	golang = true
+	var x int = 10
 	fmt.Printf("Hello World\n")
+	fmt.Println(react)
 	fmt.Println("Sum is ", add(10, 20))
+	fmt.Println(x, golang, angular, vuejs, y)
 }
 
 func add(a int, b int) int {
