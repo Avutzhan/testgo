635c723feab235d3a5ed38479c2cec1b8acc337f
Avutzhan Dautov <dautov92@list.ru>
2021-04-03 20:28:10 +0600

    simple web server

--- wiki.go
+++ wiki.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+    "fmt"
+    "log"
+    "net/http"
 )
 
-type Page struct {
-    Title string
-    Body  []byte
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
 
-func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
-}
-
+// http://localhost:8080/JohnDoe
 func main() {
-    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+    http.HandleFunc("/", handler)
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
